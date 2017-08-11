03f179e261258e92c28127b80d6e255deea97cb6
gracesojor <grace.sojor@gmail.com>
2017-08-11 03:59:13 +0000

    Clean code

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,12 +1,14 @@
 class MicropostsController < ApplicationController
-    before_action :logged_in_user,  only: [:create, :destroy]
-    before_action :correct_user,   only: :destroy
-    
+  before_action :logged_in_user,  only: [:create, :destroy]
+  before_action :correct_user,   only: :destroy
     
   def create
     @micropost = current_user.microposts.build(micropost_params)
     if @micropost.save
       flash[:success] = "Micropost created!"
+      
+      @micropost_count = Micropost.count
+      
       redirect_to root_url
     else
       @feed_items = []
